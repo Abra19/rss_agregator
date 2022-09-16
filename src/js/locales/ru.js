4d37af8b6a72291caf387724804ebf9fbbbfe205
elena_abrarova <evabrarova@gmail.com>
2022-09-16 18:51:46 +0300

    Seventh step: texts for i18next

--- src/js/locales/ru.js
+++ src/js/locales/ru.js
@@ -1,8 +1,8 @@
 export default {
   translation: {
     errors: {
-      invalidUrl: 'Ссылка должна быть валидным url!',
-      emptyField: 'Заполните это поле',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      emptyField: 'Не должно быть пустым',
       existingUrl: 'RSS уже существует',
       networkError: 'Ошибка сети',
       parseError: 'Ресурс не содержит валидный RSS',
